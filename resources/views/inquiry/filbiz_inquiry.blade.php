a9927d82460005d8ae242757da17fbe2fbd6420f
Hp <aljonrojas01@gmail.com>
2026-04-28 16:01:18 +0800

    new

--- resources/views/inquiry/filbiz_inquiry.blade.php
+++ resources/views/inquiry/filbiz_inquiry.blade.php
@@ -4,8 +4,7 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}"></script>
+@vite(['resources/js/app.js'])
 
 <link rel="stylesheet" href="{{ asset('css/filbiz.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
